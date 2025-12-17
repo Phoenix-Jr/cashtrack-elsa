4c4fe7aabd64eb64072903fafb8274040bd491d3
unknown <ntsibahdonald@gmail.com>
2025-12-17 05:50:24 +0100

    .

--- cash-track-react/vite.config.ts
+++ cash-track-react/vite.config.ts
@@ -3,17 +3,27 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tsconfigPaths(),
+    react({
+      jsxRuntime: "automatic",
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
     extensions: [".mjs", ".js", ".mts", ".ts", ".jsx", ".tsx", ".json"],
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
   },
 });
 
